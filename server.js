f4193d0d1a3f44beaba41424d65e49be3a54e6a0
Sahebjot Singh <sahebjot94@gmail.com>
2014-12-04 21:14:43 +0530

    retry connection bug fix,added transport

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http,{'transports':['websocket','polling']});
 
 //middleware
 var bodyParser = require('body-parser');
